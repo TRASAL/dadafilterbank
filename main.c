a2a459a3fbdf7137950848489c5c224f4c057970
Jisk Attema <j.attema@esciencecenter.nl>
2017-07-03 11:59:05 +0200

    First version of full StokesI to filterbank writer

--- main.c
+++ main.c
@@ -3,59 +3,39 @@
  *
  * Purpose: connect to a ring buffer and create Sigproc output per TAB on request
  * 
- * Depending on science case and mode, a ringbuffer page is interpreted as an array of Stokes IQUV:
- *   [tab=NTABS (12)][time=(25000 or 12500)][the 4 components IQUV][1536 channels]
+ *    A ringbuffer page is interpreted as an array of Stokes I:
+ *    [NTABS, NCHANNELS, padded_size] = [12, 1536, > 25000]
  *
- * Written for the AA-Alert project, ASTRON
+ *    Written for the AA-Alert project, ASTRON
  *
  * Author: Jisk Attema, Netherlands eScience Center
  * Licencse: Apache v2.0
- *
- * Inspired by the dada_dbevent.c code from PSRDADA: http://psrdada.sourceforge.net/index.shtml
- * I decided to do this one because:
- *  * the large amount of data flowing around. 
- *    Using the original code would require an extra ringbuffer instance to hold the data, and another memory copy.
- *  * now using a simple unix socket instead of a TCP/IP socket. (so no interaction with the network stack)
- *  * much of the code was already written anyways
- *
- * communicate with the unix datagram socket via fi. netcat:
- * echo 'dump' | nc -uU <socket>
- *
- * TODO: smarter dumping. Now, whenever a signal is received, we dump 1 page and overwrite existing data
  */
-#define _GNU_SOURCE
-#include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include <getopt.h>
 #include <errno.h>
-#include <poll.h>
-#include <sys/un.h>
-#include <netdb.h>
+#include <signal.h>
 
 #include "dada_hdu.h"
 #include "ascii_header.h"
 #include "filterbank.h"
 
-#define SOCKBUFSIZE 16384 // Buffer size of socket 16kB
-
 FILE *runlog = NULL;
 #define LOG(...) {fprintf(stdout, __VA_ARGS__); fprintf(runlog, __VA_ARGS__); fflush(stdout);}
 
-#define NTABS 12
-int output[NTABS];
-
-#define CACHE_SIZE 10
-unsigned char *cache_pages[CACHE_SIZE];
-int cache_start, cache_end;
+#define NCHANNELS 1536
+#define MAXTABS 12
+int output[MAXTABS];
 
 unsigned int nchannels;
 float min_frequency;
 float channel_bandwidth;
 float tsamp;
 int ntimes;
+int ntabs;
 
 /**
  * Open a connection to the ringbuffer
@@ -118,7 +98,7 @@ dada_hdu_t *init_ringbuffer(char *key) {
  * Print commandline options
  */
 void printOptions() {
-  printf("usage: dadafilterbank -c <science case> -k <hexadecimal key> -l <logfile> -s <start packet number> -t <trigger socket> -n <filename prefix for dumps>\n");
+  printf("usage: dadafilterbank -c <science case> -m <science mode> -k <hexadecimal key> -l <logfile> -s <start packet number> -b <padded_size> -n <filename prefix for dumps>\n");
   printf("e.g. dadafits -k dada -l log.txt\n");
   return;
 }
@@ -127,16 +107,23 @@ void printOptions() {
  * Parse commandline
  */
 void parseOptions(int argc, char *argv[],
-    char **key, int *science_case, unsigned long *startpacket, char **trigger_socket, char **prefix, char **logfile) {
+    char **key, int *science_case, int *science_mode, unsigned long *startpacket, int *padded_size, char **prefix, char **logfile) {
   int c;
-  int setk=0, setl=0, setc=0, sets=0, sett=0, setn=0;
-  while((c=getopt(argc,argv,"c:k:l:s:t:n:"))!=-1) {
+  int setk=0, setb=0, setl=0, setc=0, setm=0, sets=0, setn=0;
+  while((c=getopt(argc,argv,"b:c:m:k:l:s:n:"))!=-1) {
     switch(c) {
       // -k <hexadecimal_key>
       case('k'):
         *key = strdup(optarg);
         setk=1;
         break;
+
+      // -b padded_size (bytes)
+      case('b'):
+        *padded_size = atoi(optarg);
+        setb=1;
+        break;
+
 
       // -l log file
       case('l'):
@@ -154,10 +141,14 @@ void parseOptions(int argc, char *argv[],
         }
         break;
 
-      // -t <trigger socket>
-      case('t'):
-        sett=1;
-        *trigger_socket = strdup(optarg);
+      // -m <science mode>
+      case('m'):
+        setm=1;
+        *science_mode = atoi(optarg);
+        if (*science_mode != 0 && *science_mode != 1) {
+          printOptions();
+          exit(0);
+        }
         break;
 
       // -s start packet number
@@ -166,7 +157,7 @@ void parseOptions(int argc, char *argv[],
         sets=1;
         break;
 
-      // -n <trigger socket>
+      // -n <filename prefix>
       case('n'):
         setn=1;
         *prefix = strdup(optarg);
@@ -179,21 +170,21 @@ void parseOptions(int argc, char *argv[],
   }
 
   // All arguments are required
-  if (!setk || !setl || !setc || !sets || !sett || !setn) {
+  if (!setk || !setl || !setb || !setc || !setm || !sets || !setn) {
     printOptions();
     exit(EXIT_FAILURE);
   }
 }
 
-void open_files(char *prefix, int page_count) {
+void open_files(char *prefix) {
   int tab; // tight array beam
 
   // 1 page => 1024 microseconds
   // startpacket is in units of 1.28 us since UNIX epoch
   //
-  for (tab=0; tab<NTABS; tab++) {
+  for (tab=0; tab<ntabs; tab++) {
     char fname[256];
-    snprintf(fname, 256, "%s_%06i_%02i.fil", prefix, page_count, tab + 1);
+    snprintf(fname, 256, "%s_%02i.fil", prefix, tab + 1);
 
     // open filterbank file
     output[tab] = filterbank_create(
@@ -211,60 +202,48 @@ void open_files(char *prefix, int page_count) {
       min_frequency,          // double fch1,
       -1 * channel_bandwidth, // double foff,
       nchannels, // int nchans,
-      NTABS,     // int nbeams,
+      ntabs,     // int nbeams,
       tab + 1,   // int ibeam, TODO: start at 1?
-      4          // int nifs
+      1          // int nifs
     );
-
-    // Allocate for 8 pages IQUV for 1 TAB
-    // https://blog.plenz.com/2014-04/so-you-want-to-write-to-a-file-real-fast.html
-    if(fallocate(output[tab], 0, 0, 1228800391) == -1 && errno == EOPNOTSUPP) {
-      ftruncate(output[tab], 1228800391);
-    }
   }
 }
 
 void close_files() {
-  int tab; // tight array beam
+  int tab;
 
-  for (tab=0; tab<NTABS; tab++) {
+  for (tab=0; tab<ntabs; tab++) {
     filterbank_close(output[tab]);
   }
 }
 
-int unix_domain_socket (char *socket_path) {
-  struct sockaddr_un addr;
-  char buf[100];
-  int fd,cl,rc;
-
-  if ( (fd = socket(AF_UNIX, SOCK_DGRAM, 0)) == -1) {
-    perror("socket error");
-    exit(-1);
+/**
+ * Catch SIGINT then sync and close files before exiting
+ */
+void sigint_handler (int sig) {
+  LOG("SIGINT received, aborting\n");
+  int i;
+  for (i=0; i<ntabs; i++) {
+    if (output[i]) {
+      fsync(output[i]);
+      filterbank_close(output[i]);
+    }
   }
-
-  memset(&addr, 0, sizeof(addr));
-  addr.sun_family = AF_UNIX;
-  strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
-  unlink(socket_path);
-
-  if (bind(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
-    perror("bind error");
-    exit(-1);
-  }
-
-  return fd;
+  exit(EXIT_FAILURE);
 }
+
 
 int main (int argc, char *argv[]) {
   char *key;
   char *logfile;
   int science_case;
+  int science_mode;
+  int padded_size;
   unsigned long startpacket;
-  char *trigger_socket;
-  char *dump_prefix;
+  char *file_prefix;
 
   // parse commandline
-  parseOptions(argc, argv, &key, &science_case, &startpacket, &trigger_socket, &dump_prefix, &logfile);
+  parseOptions(argc, argv, &key, &science_case, &science_mode, &startpacket, &padded_size, &file_prefix, &logfile);
 
   // set up logging
   if (logfile) {
@@ -286,113 +265,59 @@ int main (int argc, char *argv[]) {
     tsamp = 0.00004096;
     ntimes = 25000;
   }
+  ntabs = 12; // TODO: depends on science_mode?
 
   LOG("dadafilterbank version: " VERSION "\n");
   LOG("Science case = %i\n", science_case);
   LOG("Sampling time [s] = %f\n", tsamp);
-  LOG("Trigger socket = %s\n", trigger_socket);
-  LOG("Dump prefix = %s\n", dump_prefix);
+  LOG("Filename prefix = %s\n", file_prefix);
+
+  // create filterbank files, and close files on C-c
+  open_files(file_prefix);
+  signal(SIGINT, sigint_handler);
 
   // connect to ring buffer
   dada_hdu_t *ringbuffer = init_ringbuffer(key);
   ipcbuf_t *data_block = (ipcbuf_t *) ringbuffer->data_block;
   ipcio_t *ipc = ringbuffer->data_block;
 
-  // open trigger socket
-  int trigger_fd = unix_domain_socket(trigger_socket);
-  if (trigger_fd < 0) { 
-    LOG("ERROR opening trigger socket, error code %i\n", trigger_fd);
-    exit(EXIT_FAILURE);
-  }
-
   // for interaction with ringbuffer
   uint64_t bufsz = ipc->curbufsz;
-  uint64_t full_bufs = 0;
   char *page = NULL;
+
+  // for processing a page
+  int tab, channel, time;
+  char *buffer = malloc(ntimes * NCHANNELS * sizeof(char));
+
   int page_count = 0;
+  int quit = 0;
+  while(!quit && !ipcbuf_eod(data_block)) {
 
-  // for interaction with the trigger socket
-  char message[256];
-  message[255] = '\0';
-  struct pollfd p;
-  p.fd = trigger_fd;
-  p.events = POLLIN;
-  p.revents = 0;
-
-  // to deal with triggers
-  int tab; // tight array beam
-  int to_write = 0; // pages left to write
-  int read_count;
-
-  // get the first page
-  page = ipcbuf_get_next_read(data_block, &bufsz);
-  if (! page) {
-    LOG("ERROR ipcbuf_get_next_read failed");
-    exit(EXIT_FAILURE);
-  }
-
-  while(1) {
-    p.revents = 0;
-    int rc = poll(&p, 1, 50); // wait 50 miliseconds
-
-    if (rc < 0) {
-      // problem with poll call
-      LOG("ERROR poll failed: %s\n", strerror(errno));
-      exit(EXIT_FAILURE);
-    }
-    // else if (rc == 0) {
-    //   // select timed out
-    // }
-    else if (rc > 0) {
-      // we received something
-      read(trigger_fd, message, 254);
-      LOG("Received trigger: '%s'\n", message);
-      if (to_write == 0) {
-        // we were not writing, open files, and prepare to write 8 samples to disk
-        to_write = 8;
-        open_files(dump_prefix, page_count);
-      } else if (to_write > 0) {
-        // we were writing to disk, but another trigger happened
-        // reset the write count to 8 again
-        to_write = 0;
-      }
-    }
-
-    full_bufs = ipcbuf_get_nfull(data_block);
-    if (full_bufs >= 4) {
-      // The delay has reached sufficient length,
-      // process the next page to prevent the ringbuffer from overflowing
-
-      // Check if this page needs to be saved
-      if (to_write > 0) {
-        // write this page to disk, and decrease write counter
-        to_write--;
-
-        for (tab=0; tab<NTABS; tab++) {
-          // SAMPLE * IF * NCHAN
-          write(output[tab], &page[tab*ntimes*4*1536], sizeof(char) * 1 * ntimes * 4 * 1536);
-          // see here: http://lkml.iu.edu/hypermail/linux/kernel/1005.2/01845.html
+    page = ipcbuf_get_next_read(data_block, &bufsz);
+    if (! page) {
+      quit = 1;
+    } else {
+      // page [NTABS, NCHANNELS, time(padded_size)]
+      // file [time, NCHANNELS]
+      for (tab = 0; tab < ntabs; tab++) {
+        for (channel = 0; channel < NCHANNELS; channel++) {
+          for (time = 0; time < ntimes; time++) {
+            buffer[time*NCHANNELS+channel] = page[(tab*NCHANNELS + channel) * padded_size + time];
+          }
         }
-
-        if (to_write == 0) {
-          // done writing
-          close_files();
-        }
+        write(output[tab], buffer, sizeof(char) * ntimes * NCHANNELS);
       }
-
-      // proceed to next page
       ipcbuf_mark_cleared((ipcbuf_t *) ipc);
-      page = ipcbuf_get_next_read(data_block, &bufsz);
-      if (! page) {
-        LOG("ERROR ipcbuf_get_next_read failed");
-        exit(EXIT_FAILURE);
-      }
       page_count++;
     }
-    // TODO: process remaining pages after a 'quit' has been triggered
+  }
+
+  if (ipcbuf_eod(data_block)) {
+    LOG("End of data received\n");
   }
 
   dada_hdu_unlock_read(ringbuffer);
   dada_hdu_disconnect(ringbuffer);
+  free(buffer);
   LOG("Read %i pages\n", page_count);
 }
