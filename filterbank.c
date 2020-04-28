0cfc018a0f4dbfe056d4010d46e4ca0357a93ff2
Jisk Attema <j.attema@esciencecenter.nl>
2020-04-28 10:43:46 +0200

    clean up code documentation

--- filterbank.c
+++ filterbank.c
@@ -1,33 +1,3 @@
-/**
- * Filterbank header from page 4 of http://sigproc.sourceforge.net/sigproc.pdf, retreived 2017-05-31
- *
- * telescope id (int): 0=fake data; 1=Arecibo; 2=Ooty... others to be added
- * machine id (int): 0=FAKE; 1=PSPM; 2=WAPP; 3=OOTY... others to be added
- * data type (int): 1=filterbank; 2=time series... others to be added
- * rawdatafile (char []): the name of the original data file
- * source name (char []): the name of the source being observed by the telescope
- * barycentric (int): equals 1 if data are barycentric or 0 otherwise
- * pulsarcentric (int): equals 1 if data are pulsarcentric or 0 otherwise
- * az start (double): telescope azimuth at start of scan (degrees)
- * za start (double): telescope zenith angle at start of scan (degrees)
- * src raj (double): right ascension (J2000) of source (hhmmss.s)
- * src dej (double): declination (J2000) of source (ddmmss.s)
- * tstart (double): time stamp (MJD) of first sample
- * tsamp (double): time interval between samples (s)
- * nbits (int): number of bits per time sample
- * nsamples (int): number of time samples in the data file (rarely used any more)
- * fch1 (double): centre frequency (MHz) of first filterbank channel
- * foff (double): filterbank channel bandwidth (MHz)
- * FREQUENCY START (character): start of frequency table (see below for explanation)
- * fchannel (double): frequency channel value (MHz)
- * FREQUENCY END (character): end of frequency table (see below for explanation)
- * nchans (int): number of filterbank channels
- * nifs (int): number of seperate IF channels
- * refdm (double): reference dispersion measure (cm−3 pc)
- * period (double): folding period (s)
- *
- */
-
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -81,27 +51,33 @@ int filterbank_create(
     int nifs) {
   int fd = open(file_name, O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
 
+  // Filterbank header from page 4 of http://sigproc.sourceforge.net/sigproc.pdf, retreived 2017-05-31
   put_raw_string(fd, "HEADER_START");
   put_int(fd, "telescope_id", telescope_id);
   put_int(fd, "machine_id", machine_id);
-  put_int(fd, "data_type", 1); // 1: filterbank data, 2: time series dada, DM=0
-  //put_string(fd, "rawdatafile", file_name); // can be longer than 80 characters, which several readers  cannot handle. filterbank file is valid without this field
-  put_string(fd, "source_name", source_name);
+  put_int(fd, "data_type", 1); // 1: filterbank data, 2: time series dada, DM=0...
+
+  // rawdatafile (char []): the name of the original data file
+  // In our case, this can be longer than 80 characters, which several readers cannot handle.
+  // A filterbank file is valid without this field
+  // put_string(fd, "rawdatafile", file_name);
+
+  put_string(fd, "source_name", source_name); // the name of the source being observed by the telescope
   put_int(fd, "barycentric", 0); // 0: no, 1: yes
   put_int(fd, "pulsarcentric", 0); // 0: no, 1: yes
-  put_double(fd, "az_start", az_start);
-  put_double(fd, "za_start", za_start);
-  put_double(fd, "src_raj", src_raj);
-  put_double(fd, "src_dej", src_dej);
-  put_double(fd, "tstart", tstart);
-  put_double(fd, "tsamp", tsamp);
-  put_int(fd, "nbits", nbits);
-  put_double(fd, "fch1", fch1);
-  put_double(fd, "foff", foff);
-  put_int(fd, "nchans", nchans);
+  put_double(fd, "az_start", az_start); // telescope azimuth at start of scan (degrees)
+  put_double(fd, "za_start", za_start); // telescope zenith angle at start of scan (degrees)
+  put_double(fd, "src_raj", src_raj); // right ascension (J2000) of source (hhmmss.s)
+  put_double(fd, "src_dej", src_dej); // declination (J2000) of source (ddmmss.s)
+  put_double(fd, "tstart", tstart); // time stamp (MJD) of first sample
+  put_double(fd, "tsamp", tsamp); // time interval between samples (s)
+  put_int(fd, "nbits", nbits); // number of bits per time sample
+  put_double(fd, "fch1", fch1); // centre frequency (MHz) of first filterbank channel
+  put_double(fd, "foff", foff); // filterbank channel bandwidth (MHz)
+  put_int(fd, "nchans", nchans); // number of filterbank channels
   put_int(fd, "nbeams", nbeams); // NOT DOCUMENTED BUT IN USE IN THE SIGPROC CODE
   put_int(fd, "ibeam", ibeam); // NOT DOCUMENTED BUT IN USE IN THE SIGPROC CODE
-  put_int(fd, "nifs", nifs);
+  put_int(fd, "nifs", nifs); // number of seperate IF channels
   put_raw_string(fd, "HEADER_END");
 
   return fd;
